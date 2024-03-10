57bad2d80dcc86696e42d79e132dac7bdee86c4f
Takahito Nakano <kano@ponko2.jp>
2024-03-10 21:23:21 +0900

    build: TailwindCSSの設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
+        "plugin:tailwindcss/recommended",
         "prettier",
       ],
       settings: {
