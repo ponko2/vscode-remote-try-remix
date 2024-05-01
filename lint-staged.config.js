2debd4ff8a1cfebe24c0fa3c5930b79877f165c8
Takahito Nakano <kano@ponko2.jp>
2024-05-02 04:27:09 +0900

    build: Prismaの設定を調整

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
 export default {
   "*.{css,json,scss,yml}": "prettier --write",
   "*.{js,jsx,cjs,mjs,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "schema.prisma": (filenames) =>
+    filenames.flatMap((filename) => [
+      `prisma format --schema=${filename}`,
+      `prisma-case-format --file ${filename}`,
+    ]),
 };
--- prisma/migrations/20240310125052_init/migration.sql
+++ prisma/migrations/20240310125052_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Todo" (
+CREATE TABLE "todo" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "completed" BOOLEAN NOT NULL DEFAULT false
