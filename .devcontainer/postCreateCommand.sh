6b3a156e0ec968d7e23a8f46cd751066b70edd78
Takahito Nakano <kano@ponko2.jp>
2024-06-07 19:27:23 +0900

    chore: DevContainerの設定を更新

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
-pnpm config set store-dir ~/.local/share/pnpm/store
+git config --global --add safe.directory /workspaces/vscode-remote-try-remix
+
 npm uninstall -g pnpm
 sudo corepack enable pnpm
+corepack install --global pnpm
+pnpm config set store-dir ~/.local/share/pnpm/store
+
+cp .env{.example,}
+pnpm install --force
+pnpm exec prisma migrate dev
