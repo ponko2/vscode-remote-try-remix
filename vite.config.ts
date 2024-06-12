26e42e4cbeee11e29f38a48f2de4a4ef05cedbec
Takahito Nakano <kano@ponko2.jp>
2024-06-12 22:14:12 +0900

    build: Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,9 @@ export default defineConfig({
       remix({
         future: {
           unstable_singleFetch: true,
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
         },
       }),
     !(isVitest || isStorybook) &&
