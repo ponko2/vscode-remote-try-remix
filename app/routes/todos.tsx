4855fc0080118d3cb8e212e0edd7d8ef4c9968f0
Takahito Nakano <kano@ponko2.jp>
2024-08-02 23:21:32 +0900

    refactor: Remove unnecessary argument types

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -1,13 +1,9 @@
 import { parseWithZod } from "@conform-to/zod";
-import {
-  unstable_defineAction as defineAction,
-  json,
-  type ActionFunctionArgs,
-} from "@remix-run/node";
+import { unstable_defineAction as defineAction, json } from "@remix-run/node";
 import { createTodo } from "~/.server/models/todo";
 import { createTodoSchema } from "~/schemas/todo";
 
-export const action = defineAction(async ({ request }: ActionFunctionArgs) => {
+export const action = defineAction(async ({ request }) => {
   const formData = await request.formData();
   const submission = parseWithZod(formData, { schema: createTodoSchema });
   if (submission.status !== "success") {
