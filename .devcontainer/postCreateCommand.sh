bd799d685ce5f88aaa75f265beec6b36b18fd9de
Takahito Nakano <kano@ponko2.jp>
2025-09-28 21:34:37 +0900

    chore: Clean up postCreateCommand.sh script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,12 +4,6 @@ set -euo pipefail
 
 git config --global --add safe.directory /workspaces/vscode-remote-try-remix
 
-npm uninstall -g pnpm
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-pnpm config set store-dir "$HOME/.local/share/pnpm/store"
-
 cp .env{.example,}
 pnpm install --force
 pnpm exec prisma migrate dev
