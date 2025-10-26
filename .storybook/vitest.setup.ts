72caa0583debe545239ce3206a30231e2906a705
Takahito Nakano <kano@ponko2.jp>
2025-10-26 19:52:55 +0900

    chore(deps): Update vitest to 4.0.3

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,6 +1,6 @@
 import { setProjectAnnotations } from "@storybook/react-vite";
-import * as projectAnnotations from "./preview";
+import * as previewAnnotations from "./preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([projectAnnotations]);
+setProjectAnnotations([previewAnnotations]);
